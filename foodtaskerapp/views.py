4ec411ef34438946211d5027b5f6b5bdecf06b05
Dmytro <dim.malashenko@gmai.com>
2018-03-29 11:59:33 -0700

    added sign-up

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -11,3 +11,29 @@ def home(request):
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
     return render(request, 'restaurant/home.html', {})
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign-up.html', {})
+    # user_form = UserForm()
+    # restaurant_form = RestaurantForm()
+    #
+    # if request.method == "POST":
+    #     user_form = USerForm(request.POST)
+    #     restaurant_form = RestaurantForm(request.POST, request.FILES)
+    #
+    #     if user_form.is_valid() and restaurant_form.is_valid():
+    #         new_user = User.objects.create_user(**user_form.cleaned_data)
+    #         new_restaurant = restaurant_form.save(commit=False)
+    #         new_restaurant.save()
+    #
+    #         login(request, authenticate(
+    #             username = user_form.cleaned_data["username"],
+    #             password = user_form.cleaned_data["password"],
+    #         ))
+    #
+    #         return redirect(restaurant_home)
+    #
+    # return render(request, 'restaurant/sign_up.html', {
+    #     "user_form": user_form,
+    #     "restaurant_form": restaurant_form
+    # })
