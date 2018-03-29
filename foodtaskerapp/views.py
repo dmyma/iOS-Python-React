61807b4de8d8ceb39748fd6391606c6865462961
Dmytro <dim.malashenko@gmai.com>
2018-03-29 11:54:58 -0700

    added login

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # from foodtaskerapp.forms import UserForm, RestaurantForm
 # from django.contrib.auth import authenticate, login
 # from django.contrib.auth.model import User
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html', {})
